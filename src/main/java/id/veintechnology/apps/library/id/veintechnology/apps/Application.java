95268df944f9447c4ccede06eb282ce315e9e38b
Dhanar J Kusuma <dhanar.januardhi.k@mail.ugm.ac.id>
2018-09-26 09:48:24 +0700

    delete test - add config jpa

--- src/main/java/id/veintechnology/apps/library/id/veintechnology/apps/Application.java
+++ src/main/java/id/veintechnology/apps/library/id/veintechnology/apps/Application.java
@@ -3,11 +3,10 @@ package id.veintechnology.apps.library.id.veintechnology.apps;
 import id.veintechnology.apps.library.id.veintechnology.apps.api.ApiConfig;
 import id.veintechnology.apps.library.id.veintechnology.apps.config.AuthenticationConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 		ApiConfig.class
 })
 @SpringBootApplication
+@EnableJpaRepositories
 public class Application {
 
 	public static void main(String[] args) {
